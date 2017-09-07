af7318f5ed06d1b30a3d433349003319cb3faebb
Naz Khan <nazmuskhan@yahoo.com>
2017-09-07 00:38:57 -0400

    adding more treasures

--- main_app/views.py
+++ main_app/views.py
@@ -2,8 +2,21 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def index(request):
-    return HttpResponse('<h1>Hello Explorers!</h1>')
-# Create your views here.
+    return render(request, 'index.html', {'treasures': treasures})
+
+class Treasure:
+    def __init__(self, name, value, material, location):
+        self.name = name
+        self.value = value
+        self.material = material
+        self.location = location
+
+
+treasures = [
+    Treasure('Gold Nugget', 500.00, 'gold', "Curly's Creek, NM"),
+    Treasure("Fool's Gold", 0, 'pyrite', "Fool's Falls, CO"),
+    Treasure("Coffee Can", 20.00, 'tin', "Acme, CA")
+]
